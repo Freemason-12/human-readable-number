c238547ad51040a93da916b4ee4b76a727e965be
Maksat <maksatr@localhost.localdomain>
2023-07-29 11:44:38 +0600

    feat: added a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(n) {
+    if (n == 0) return 'zero'
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const elev = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    switch (`${n}`.length) {
+        case 1: return ones[n]
+        case 2:
+            const fl = Math.floor(n / 10), rm = n % 10
+            return fl == 1 ? elev[rm] : `${tens[fl]}${rm == 0 ? "" : " " + ones[rm]}`
+        case 3:
+            const d = n % 100
+            const dl = Math.floor(d / 10), rn = d % 10
+            const t = dl == 1 ? " " + elev[rn] : `${dl == 0 ? "" : " " + tens[dl]}${rn == 0 ? "" : " " + ones[rn]}`
+            return `${ones[Math.floor(n / 100)]} hundred${t}`.trim()
+    }
 }
